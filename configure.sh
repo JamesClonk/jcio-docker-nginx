463a7032b0146535a82b116bec688d246ec7861c
JamesClonk <jamesclonk@jamesclonk.ch>
2015-02-21 15:27:50 +0100

    configure script

---
+++ configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -u
+
+# usage check
+if [ $# -ne 1 ]; then
+	echo "usage: $0 <JCIO_HOSTNAME>"
+	exit 1
+fi
+
+JCIO_HOSTNAME=$1
+
+for file in `ls *.conf.tmpl`; do
+	sed "s/%JCIO_HOSTNAME%/${JCIO_HOSTNAME}/g" $file > ${file%.*}
+done
+
+exit 0
